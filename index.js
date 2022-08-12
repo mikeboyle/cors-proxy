2cc716de9086546e90b2f582b58c2e4885a86d45
Mike Boyle <mikesboyle@gmail.com>
2022-08-12 11:24:52 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const PORT = process.env['PORT'] || 8000;
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch-native');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.get('*', (req, res) => {
+  // get the url we want to proxy
+  const url = req.path.slice(1);
+
+  // make the proxy request and return it to client
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
